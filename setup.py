c29850916b3f79dbf9ba24573ee1a73c3110532a
Giuliano P. Tognarelli Buono-core <giuliano150@hotmail.com>
2021-04-03 11:55:22 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pyutils',
+    version='0.1.3',
+    author="freyja-folkvangr",
+    author_email="hello@octocat.xyz",
+    description="set of util functions for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Freyja-Folkvangr/pyutils",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+ )
