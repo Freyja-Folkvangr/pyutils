ce204846a7c0d8721a2dd0ea13b7658b81a016af
Giuliano Tognarelli <gio@octocat.xyz>
2021-04-03 12:35:30 -0300

    moved package to src folder

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pyutils',
-    version='0.1.3',
+    version='0.1.4',
     author="freyja-folkvangr",
     author_email="hello@octocat.xyz",
     description="set of util functions for python",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Freyja-Folkvangr/pyutils",
-    package_dir={"": "pyutils"},
-    packages=setuptools.find_packages(where="pyutils"),
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     install_requires=[],
     classifiers=[
         "Programming Language :: Python :: 3",
